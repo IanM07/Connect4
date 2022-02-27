9da39716db2884382398e7a9f0f150ccabd003e3
IanM07 <93929476+IanM07@users.noreply.github.com>
2022-02-27 18:41:56 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package connect4;
 
 import java.awt.Color;
@@ -9,7 +5,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author under
+ * @author Ian McMullen
  */
 public class Board {
         
